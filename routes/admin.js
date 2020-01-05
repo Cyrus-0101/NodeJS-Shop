349da33a3a70c48683ced0940cb81675ba2ca6fb
Toxic254 <cyrusgichuki1998@gmail.com>
2020-01-05 19:15:10 +0300

    Changed Node Modules. Started using EJS.

--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,22 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views','add-product.html'));
+  res.render('add-product', { 
+      pageTitle: 'Add Product',
+      path: '/admin/add-product', 
+      formsCSS: true, 
+      productCSS: true, 
+      activeAddProduct: true });
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,20 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views','shop.html'));
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        activeCSS: true
+    });
 });
 
 module.exports = router;
