9d43d8e2d9be0fc7e283e26793016eaf1b7b0017
Toxic254 <cyrusgichuki1998@gmail.com>
2020-01-05 08:50:59 +0300

    Before CSS. 404 Page Successful so far.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,7 +13,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Error!<br/><hr/>Page Not found!</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(4000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
+const path =require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-    );
-    });
+    res.sendFile(path.join(rootDir, 'views','add-product.html'));
+});
 
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
